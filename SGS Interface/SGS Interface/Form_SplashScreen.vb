720bc4aa22381cde7772e13d965d0961a16aee36
John <rasetsumaru@icloud.com>
2016-09-05 23:18:18 -0300

    Thirteenth commit

--- SGS Interface/SGS Interface/Form_SplashScreen.vb
+++ SGS Interface/SGS Interface/Form_SplashScreen.vb
@@ -1,87 +1,96 @@
-﻿Public NotInheritable Class Form_SplashScreen
+﻿
+Public NotInheritable Class Form_SplashScreen
+
+#Region "Declarations"
+
+    Private Declare Auto Function WritePrivateProfileString Lib "Kernel32" (ByVal lpAppName As String, ByVal lpKeyName As String, ByVal ByVallpString As String, ByVal lpFileName As String) As Integer
+
+#End Region
+
+#Region "Form events"
 
     Private Sub Form_SplashScreen_HandleDestroyed(sender As Object, e As EventArgs) Handles Me.HandleDestroyed
 
-        Dim FileOpen As String
+        Try
 
-        FileOpen = Command()
+            Dim FileOpen As String
 
-        If Not FileOpen = "" Then
+            FileOpen = Command()
 
-            FileOpen = FileOpen.Substring(1, FileOpen.Length() - 2)
+            If Not FileOpen = "" Then
 
-            If Strings.Right(FileOpen, 5).Equals(".sgss") Or Strings.Right(FileOpen, 5).Equals(".sgsr") Then
+                FileOpen = FileOpen.Substring(1, FileOpen.Length() - 2)
 
-                FileDirectory = Strings.Left(FileOpen, FileOpen.LastIndexOf("\"))
-                FileName = Strings.Right(FileOpen, FileOpen.Length - FileOpen.LastIndexOf("\") - 1)
+                If Strings.Right(FileOpen, 5).Equals(".sgss") Or Strings.Right(FileOpen, 5).Equals(".sgsr") Then
 
-                Dim nome_arquivo_ini As String = FileDirectory & "\" & FileName
+                    FileDirectory = Strings.Left(FileOpen, FileOpen.LastIndexOf("\"))
+                    FileName = Strings.Right(FileOpen, FileOpen.Length - FileOpen.LastIndexOf("\") - 1)
 
-                Dim FirmwareData As String = ReadFileChecksum(nome_arquivo_ini, "Firmware", "FR000", SGS_Firmware.Firmware)
+                    Dim nome_arquivo_ini As String = FileDirectory & "\" & FileName
 
-                SGS_Firmware.Firmware = FirmwareData.Substring(0, FirmwareData.IndexOf(" "))
-                SGS_Firmware.ReadFirmwareLibrary()
+                    Dim FirmwareData As String = ReadFileChecksum(nome_arquivo_ini, "Firmware", "FR000", SGS_Firmware.Firmware)
 
-                If Strings.Right(FileOpen, 5).Equals(".sgss") Then
+                    SGS_Firmware.Firmware = FirmwareData.Substring(0, FirmwareData.IndexOf(" "))
+                    SGS_Firmware.ReadFirmwareLibrary()
 
-                    OpenFormSettings()
+                    If Strings.Right(FileOpen, 5).Equals(".sgss") Then
 
-                End If
+                        OpenFormSettings()
 
-                If Strings.Right(FileOpen, 5).Equals(".sgsr") Then
+                    End If
 
-                    OpenFormRecipe()
+                    If Strings.Right(FileOpen, 5).Equals(".sgsr") Then
+
+                        OpenFormRecipe()
+
+                    End If
 
                 End If
 
             End If
 
-        End If
+            Select Case FileSystem
 
-        Select Case FileSystem
+                Case 1
 
-            Case 1
+                    OpenFormRecipe()
 
-                OpenFormRecipe()
+                Case 2
 
-            Case 2
+                    OpenFormSettings()
 
-                OpenFormSettings()
+            End Select
 
-        End Select
+        Catch ex As Exception
+
+            WritePrivateProfileString("Error >> " & Format(Now, "MM/dd/yyyy"), " >> " & Format(Now, "HH:mm:ss") & " >> Erro = ", ex.Message & " - " & ex.StackTrace & " - " & ex.Source, NomeArquivoINI(DirLogsError))
+
+        End Try
 
     End Sub
-
-    'TODO: Este formulário pode ser facilmente configurado como a tela inicial da aplicação através da edição da aba "Aplicação"
-    '  no Designer de Projeto ("Propriedades" dentro do menu "Projetos").
-
 
     Private Sub Form_SplashScreen_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'Configura o texto do diálogo em tempo de execução de acordo com a informação do assembly da aplicação.  
 
-        'TODO: Personalize a informação do assembly da aplicação no painel "Aplicação" do diálogo 
-        '  propriedades do projeto (dentro do menu "Project").
+        Try
 
-        'Título da Aplicação
-        If My.Application.Info.Title <> "" Then
-            ApplicationTitle.Text = My.Application.Info.Title
-        Else
-            'Se o título da aplicação estiver faltando, utiliza o nome da aplicação sem a extensão
-            ApplicationTitle.Text = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
-        End If
+            If My.Application.Info.Title <> "" Then
+                ApplicationTitle.Text = My.Application.Info.Title
+            Else
+                ApplicationTitle.Text = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+            End If
 
-        'Formata a informação de versão utilizando o texto configurado no controlador de Versão em tempo de design como a
-        '  cadeia de caractere de formatação.  Isto facilita uma localização efetiva quando necessário.
-        '  Informação de compilação e revisão poderiam ser incluídos utilizando o seguinte código e modificando o 
-        '  texto designtime do controle de versão para "Versão {0}.{1:00}.{2}.{3}" ou algo similar.  Verifique
-        '  String.Format() na Ajuda para mais informação.
-        '
-        '    Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
+            Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor)
 
-        Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor)
+            Copyright.Text = My.Application.Info.Copyright
 
-        'Informação de Copyright
-        Copyright.Text = My.Application.Info.Copyright
+        Catch ex As Exception
+
+            WritePrivateProfileString("Error >> " & Format(Now, "MM/dd/yyyy"), " >> " & Format(Now, "HH:mm:ss") & " >> Erro = ", ex.Message & " - " & ex.StackTrace & " - " & ex.Source, NomeArquivoINI(DirLogsError))
+
+        End Try
+
     End Sub
+
+#End Region
 
 End Class
